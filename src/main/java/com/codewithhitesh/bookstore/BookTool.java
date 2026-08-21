79f4dfbe4d1b4be2bc3b828a3c9f516796d62e6e
Hitesh S <hitesh.codingsolutions@gmail.com>
2026-08-21 22:50:47 +0530

    feat: implement RAG pipeline and Spring AI tool calling with local embeddings

--- src/main/java/com/codewithhitesh/bookstore/BookTool.java
+++ src/main/java/com/codewithhitesh/bookstore/BookTool.java
@@ -14,14 +14,14 @@ public class BookTool {
     }
 
 
-    // Stock calculator
+    // Stock calculator Tool
     @Tool(description = "Checks the stock quantity of a book using its name")
     public String getBookAvailability(String bookName) {
 
         return userBookService.checkBookStock(bookName);
     }
 
-    // Price calculator
+    // Price calculator Tool
     @Tool(description = "Checks the selling price of a book using its name")
     public String getBookPrice(String bookName) {
 
